39c890f846b2cfe1c56411c477476dc3e5bfe1a2
Emma Davenport <Davenport.Physics@gmail.com>
2019-05-14 22:07:41 +0200

    Removed finding file size function. Load entire cartridge using C++ fstream

--- src/memory.cpp
+++ src/memory.cpp
@@ -1,24 +1,41 @@
 
 #include <cstdio>
+#include <cstdlib>
 #include <cstring>
+#include <fstream>
 
 #include "memory.hh"
 #include "shared.hh"
 
-char name[17];
+#define MAX_CARTRIDGE_SIZE 8388608
+
+char name[17]; 
+long cartridge_size;
 uint_8 *cartridge_memory;
 
 CartridgeType cartridge;
 GameboyType gameboy;
 
-long int GetFileSize(char *cartridge) 
+long GetFileSize(char *cartridge) 
 {
+	// TODO or maybe not?
+	// Value obtained is different than the amount of bytes read.
 
 	FILE *fp = fopen(cartridge, "rb");
+	
+	if (fp == NULL) {
+	
+		printf("Unable to open %s\n", cartridge);
+		exit(0);
+
+	}
+
 	fseek(fp, 0, SEEK_END);
 
-	long int filesize = ftell(fp);
+	unsigned long filesize = ftell(fp);
 	fclose(fp);
+
+	printf("detected catridge file size = %d bytes\n", filesize);
 
 	return filesize;
 
@@ -27,9 +44,8 @@ long int GetFileSize(char *cartridge)
 void InitCartridgeMemory(char *cartridge) 
 {
 
-	long int file_size = GetFileSize(cartridge);
-	cartridge_memory   = new uint_8[file_size];
-	memset(cartridge_memory, 0, file_size);
+	cartridge_memory = new uint_8[MAX_CARTRIDGE_SIZE];
+	memset(cartridge_memory, 0, MAX_CARTRIDGE_SIZE);
 
 }
 
@@ -38,17 +54,18 @@ void LoadCartridge(char *cartridge)
 
 	InitCartridgeMemory(cartridge);
 
-	FILE *fp = fopen(cartridge, "rb");
-	unsigned int idx = 0;
-	unsigned char c;
-	while ((c = getc(fp)) != EOF) {
-	
-		cartridge_memory[idx] = c;
-		idx++;
-	
-	}
+	std::ifstream cartridge_file_handle;
+	cartridge_file_handle.open(cartridge, std::ifstream::in | std::ifstream::binary);
 
-	fclose(fp);
+	long idx = 0;
+	uint_8 c;
+	while (!cartridge_file_handle.eof()) {
+	
+		cartridge_memory[idx] = cartridge_file_handle.get();
+		idx++;
+
+	}
+	cartridge_file_handle.close();
 
 }
 
